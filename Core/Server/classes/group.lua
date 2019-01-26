0685a5df42c7a3b67499459081d0362d3d50c542
xrayhunter <3002612+xrayhunter@users.noreply.github.com>
2019-01-25 21:23:03 -0800

    V1.0.0.4

--- Core/Server/classes/group.lua
+++ Core/Server/classes/group.lua
@@ -1,9 +1,25 @@
 
+local groups = {}
+
 -- Constructs Group Data
-local function CreateGroup()
-    
+local function CreateGroup(name, inherits, permissions)
+
 end
 
-AddEventHandler("rac:addGroup", function(name, inherits, permissions)
+local function loadGroups()
 
+end
+
+AddEventHandler("rac:addGroup", function(name, callback) end)
+
+AddEventHandler("rac:setGroupInherits", function(name, inherits) end)
+
+AddEventHandler("rac:setGroupPerms", function(name, perms) end)
+
+AddEventHandler("rac:setGroupNoTarget", function(name, noTarget) end)
+
+AddEventHandler("rac:setGroupPermNoTarget", function(name, perm, noTarget) end)
+
+AddEventHandler("rac:getGroups", function(callback) 
+    callback(groups);
 end)
